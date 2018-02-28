358acc90b99d0f8625a71d83ecfa5b335b14a1e9
sergiperez <sergi.perez@iesjoandaustria.org>
2018-02-28 11:36:43 +0100

    TDD. Cas prova: 1 -> I

---
+++ romans.py
@@ -0,0 +1,14 @@
+'''
+http://codingdojo.org/kata/RomanNumerals/
+https://remonsinnema.com/2011/12/05/practicing-tdd-using-the-roman-numerals-kata/
+'''
+def transformar(nombreArab):
+    '''
+     >>> transformar(1)
+     'I'
+    '''
+    return 'I' ; 
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
